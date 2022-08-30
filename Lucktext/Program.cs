0f39126802906e3f5abf5cac1b341abe49686217
Daniel Taherkhanian <daniel.taherkhanian@elev.ga.ntig.se>
2022-08-30 10:29:46 +0200

    sgfbdghmrhfyj

---
+++ Lucktext/Program.cs
@@ -0,0 +1,15 @@
+﻿string name;
+string age;
+string distance_time;
+
+Console.WriteLine("What is your name?");
+name = Console.ReadLine();
+
+Console.WriteLine("How old are you?");
+age = Console.ReadLine();
+
+Console.WriteLine("How long does it take to get to your school if you travel by bus?");
+distance_time = Console.ReadLine();
+
+Console.WriteLine($"Thank you for bringing this up {name}. Because of the fact that you are {age} years old and that you have to travel {distance_time} minutes in order to arrive at the school, we have decided to give you permsission to arrive at your classes later than everyone else.");
+Console.ReadLine();
